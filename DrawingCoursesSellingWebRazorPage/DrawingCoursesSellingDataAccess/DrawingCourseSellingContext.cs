618235b538c8602f75d9a21f66f3f06465fdbf26
Lê Tuấn Kiệt <tuankietgg29@gmail.com>
2023-10-18 21:30:36 +0700

    Update Project

--- DrawingCoursesSellingWebRazorPage/DrawingCoursesSellingDataAccess/DrawingCourseSellingContext.cs
+++ DrawingCoursesSellingWebRazorPage/DrawingCoursesSellingDataAccess/DrawingCourseSellingContext.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using BusinessObject;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
-namespace BusinessObject;
+namespace DataAccess;
 
 public partial class DrawingCourseSellingContext : DbContext
 {
@@ -40,8 +42,17 @@ public partial class DrawingCourseSellingContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);Database=DrawingCourseSelling;Uid=sa;Pwd=12345;TrustServerCertificate=true;");
+        => optionsBuilder.UseSqlServer(GetConnectionString());
+
+    private string GetConnectionString()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+        var strConn = config["ConnectionStrings:DefaultConnection"];
+        return strConn;
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
